efc4f2aa26ee926e7a52aa166ca9eb47f99e3232
Michael Ohayon <mohayon75@gmail.com>
2017-04-12 22:31:13 +0200

    update path

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --device /dev/snd -v `pwd`:/pwd -w /pwd -it mikkl/rpi-raspbian-baby-cry-detection $1
+docker run --rm --device /dev/snd -v `pwd`:/opt/baby_cry -w /opt/baby_cry -it mikkl/rpi-raspbian-baby-cry-detection $1
