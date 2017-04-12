dca96a5d3d296b2725eec0c49aa99c7bd8e993c2
Michael Ohayon <mohayon75@gmail.com>
2017-04-12 22:37:23 +0200

    up

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm --device /dev/snd -v `pwd`:/opt/baby_cry -w /opt/baby_cry -it mikkl/rpi-raspbian-baby-cry-detection $1
+docker run --rm --device /dev/snd -v `pwd`:/opt/baby_cry -w /opt/baby_cry -it mikkl/rpi-raspbian-baby-cry-detection "bash script/run_crying_detection.sh"
